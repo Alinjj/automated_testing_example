ecca620764c3f2d932a31116142f4230ed8e4a7e
Mogilat Igor <deadm7us@mail.ru>
2023-03-20 22:42:29 +0800

    Add new options in conftest.py

--- test_different_languages_ui/test_items.py
+++ test_different_languages_ui/test_items.py
@@ -7,5 +7,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_btn(driver):
     driver.get(link)
-    btn = WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.CLASS_NAME,'btn-primary')))
-    assert len(btn) > 0,'button not found'
+    assert WebDriverWait(driver,30).until(EC.element_to_be_clickable((By.CLASS_NAME,'btn-primary'))), 'btn not found'
